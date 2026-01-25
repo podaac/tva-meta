95fecaa0dc4bbe8d4669e9489367e4cad8443e04
thomas loubrieu <thomas.loubrieu@jpl.nasa.gov>
2026-01-25 12:59:53 -0800

    switch to DEBUG level

--- src/sync_projects/sync_attributes.py
+++ src/sync_projects/sync_attributes.py
@@ -5,7 +5,7 @@ import logging
 
 # Configure logging
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     handlers=[logging.StreamHandler(sys.stdout)]
 )
